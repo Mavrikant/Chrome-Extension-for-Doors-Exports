48dce5535b704762a211798a74c38e9edafee579
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-06-15 13:28:00 +0300

    prevent working on folded field

--- after.js
+++ after.js
@@ -1,11 +1,10 @@
 // this code will be executed after page load
-(function() {
-  console.log('after.js executed');
-})();
 
 var element = document.getElementById('customfield_11112-val');
-if(element){
-  if (!element.innerText.includes("http")){
-    element.innerHTML = '<a href="http://esen-1332:1111'+element.innerHTML.trim()+'.html">'+element.innerHTML+'</a>'
+if (element) {
+  if (element.childNodes.length === 1) {
+    if (!element.innerText.includes("http")) {
+      element.innerHTML = '<a href="http://esen-1332:1111' + element.innerHTML.trim() + '.html">' + element.innerHTML + '</a>'
+    }
   }
 }
