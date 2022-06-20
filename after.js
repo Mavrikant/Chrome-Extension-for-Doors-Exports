17490ef31e984e3e11922122104eed80cf23e5d9
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-06-20 23:42:19 +0300

    images moved into folder

--- after.js
+++ after.js
@@ -3,7 +3,7 @@ chrome.storage.sync.get({ "serverPath": "http://esen-1332:1111/" }, function (re
   if (element) {
     if (element.childNodes.length === 1) {
       if (!element.innerText.includes("http")) {
-        element.innerHTML = '<a href="' + result.serverPath + element.innerHTML.trim() + '.html"><img id="Doors" height="20px" src="' + chrome.runtime.getURL("doors.png") + '">' + element.innerHTML + '</a>'
+        element.innerHTML = '<a href="' + result.serverPath + element.innerHTML.trim() + '.html"><img id="Doors" height="20px" src="' + chrome.runtime.getURL("images/doors.png") + '">' + element.innerHTML + '</a>'
       }
     }
   }
